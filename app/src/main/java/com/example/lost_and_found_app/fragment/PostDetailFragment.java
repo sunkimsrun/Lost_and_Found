dea87a861620b0d1ebe3a6d693f7ca627aa30bdd
Shongkhoung <roeunsoungkhoung@gmail.com>
2025-04-26 01:33:05 +0700

    feat: add post detail and view item fragments with UI enhancements
    -   Added `PostDetailFragment` and `ViewItemFragment` with corresponding layouts (`fragment_post_detail.xml`, `fragment_view_item.xml`).
    -   Added new drawables: `background.xml`, `baseline_contact_phone_24.xml`, `bg_saved.xml`, `component_52.xml`, `ico_bookmark.xml`, `icon_return_up_back.xml`, `mail.xml`, and `tra.jpg`.
    -   Modified `timer.xml` to adjust its size and updated the `post_divider.xml` to have a height of 2dp.
    -   Updated layout elements in `fragment_post_detail.xml` including adding a back button, found checkbox, image view, user information, post details, contact information, and a call now button.
    - Added user image.

---
+++ app/src/main/java/com/example/lost_and_found_app/fragment/PostDetailFragment.java
@@ -0,0 +1,22 @@
+package com.example.lost_and_found_app.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class PostDetailFragment extends Fragment {
+
+    public PostDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_post_detail, container, false);
+    }
+}
